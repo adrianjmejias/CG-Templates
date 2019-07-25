706026addbb142f06349006ab3bcab3ebd7932f6
adrian <adrianjmejias@gmail.com>
2019-07-25 06:21:05 -0400

    went bananas

--- PressF/src/Camera.cpp
+++ PressF/src/Camera.cpp
@@ -3,18 +3,7 @@
 extern double deltaTime;
 
 
-Camera::Camera(GameObject & go, Transform & t, Vec3 position, Vec3 up, float yaw, float pitch)
-	: Component(go, t),
-	Front(Vec3(0.0f, 0.0f, -1.0f)),
-	speed(SPEED), sensitivity(SENSITIVITY),
-	fov(ZOOM)
-{
-	transform.SetPosition(position);
-	WorldUp = up;
-	Yaw = yaw;
-	Pitch = pitch;
-	updateCameraVectors();
-}
+
 
 Camera::~Camera()
 {
@@ -28,88 +17,15 @@ void Camera::Update() {
 // Inherited via Component
 
 void Camera::HandleEvent(const SDL_Event & e) {
-	if (mainCamera != this) return;
-	if (!SDL_GetRelativeMouseMode()) return;
-	if (e.type == SDL_EventType::SDL_KEYDOWN) {
-		//if (e.key.keysym.mod == SDL_Scancode::SDL_SCANCODE_LSHIFT)
-		{
-			float scaledSpeed = static_cast<float>(deltaTime) * speed;
-			if (e.key.keysym.scancode == SDL_Scancode::SDL_SCANCODE_W) {
-				transform.Translate(Front * scaledSpeed);
-			}
-			if (e.key.keysym.scancode == SDL_Scancode::SDL_SCANCODE_S) {
-				transform.Translate(-Front * scaledSpeed);
-			}
-			if (e.key.keysym.scancode == SDL_Scancode::SDL_SCANCODE_A) {
-				transform.Translate(-Right * scaledSpeed);
-			}
-			if (e.key.keysym.scancode == SDL_Scancode::SDL_SCANCODE_D) {
-				transform.Translate(Right* scaledSpeed);
-			}
-		}
-	}
-	if (e.type == SDL_EventType::SDL_MOUSEMOTION) {
-		float movX = sensitivity * e.motion.xrel * static_cast<float>(deltaTime);
-		float movY = sensitivity * e.motion.yrel * static_cast<float>(deltaTime);
 
-		float xoffset = e.motion.xrel* sensitivity;
-		float yoffset = e.motion.yrel* sensitivity;
-
-		Yaw += xoffset;
-		Pitch += yoffset;
-
-		// Make sure that when pitch is out of bounds, screen doesn't get flipped
-		//if (constrainPitch)
-		{
-			if (Pitch > 89.0f)
-				Pitch = 89.0f;
-			if (Pitch < -89.0f)
-				Pitch = -89.0f;
-		}
-		
-		transform.SetRotation(glm::degrees(Front));
-		// Update Front, Right and Up Vectors using the updated Euler angles
-		updateCameraVectors();
-	}
 }
 
-void Camera::updateCameraVectors()
+Mat4 & Camera::GetView()
 {
-	// Calculate the new Front vector
-	glm::vec3 front;
-	front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	front.y = sin(glm::radians(Pitch));
-	front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	Front = glm::normalize(front);
-	// Also re-calculate the Right and Up vector
-	Right = glm::normalize(glm::cross(Front, Transform::WorldUp()));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-	Up = glm::normalize(glm::cross(Right, Front));
+	// TODO: insert return statement here
 }
 
-Mat4 & Camera::GetView() {
-	view = glm::lookAt(transform.GetPosition(), transform.GetPosition() + Front, Up);
-	//view = transform.GetAccumulated();
-	//view = Transform::GenModel(-transform.GetPosition(), -transform.GetRotation(), transform.GetScale());
-	//return view;
-	return view;
-}
-
-Mat4 & Camera::GetProjection(ProjectionType type, int w, int h) {
-	if (type == ProjectionType::CAM_SETUP) {
-		type = (isPerspective) ? ProjectionType::PERSPECTIVE : ProjectionType::ORTHO;
-	}
-
-	switch (type)
-	{
-	case ProjectionType::PERSPECTIVE:
-		projection = glm::perspective(glm::radians(fov), static_cast<float>(w) / h, nearClippingPlane, farClippingPlane);
-		break;
-	case ProjectionType::ORTHO:
-		projection = glm::ortho(0.f, static_cast<float>(w), 0.f, static_cast<float>(h), nearClippingPlane, farClippingPlane);
-		break;
-	default:
-		break;
-	}
-
-	return projection;
+Mat4 & Camera::GetProjection(ProjectionType type, int w, int h)
+{
+	// TODO: insert return statement here
 }
--- PressF/src/Camera.h
+++ PressF/src/Camera.h
@@ -8,22 +8,6 @@ enum class ProjectionType {
 };
 class Camera;
 
-enum Camera_Movement {
-	FORWARD,
-	BACKWARD,
-	LEFT,
-	RIGHT
-};
-
-// Default camera values
-const float YAW = -90.0f;
-const float PITCH = 0.0f;
-const float SPEED = 2.5f;
-const float SENSITIVITY = 0.1f;
-const float ZOOM = 45.0f;
-
-
-
 extern Camera* mainCamera;
 
 class Camera : public Component{
@@ -37,41 +21,14 @@ public:
 	float nearClippingPlane = 0.1f;
 	float farClippingPlane = 400.f;
 
-	glm::vec3 Front;
-	glm::vec3 Up;
-	glm::vec3 Right;
-	glm::vec3 WorldUp;
-
-	float Yaw;
-	float Pitch;
-
 	Mat4 projection;
 	Mat4 view;
-
-
-	Camera::Camera(GameObject & go, Transform & t,
-		Vec3 position = Vec3(0.0f, 0.0f, 0.0f), Vec3 up = Vec3(0.0f, 1.0f, 0.0f),
-		float yaw = YAW, float pitch = PITCH);;
-
-	Camera::~Camera();
 
 	// Inherited via Component
 	virtual void Update() override;
 
-	//// Inherited via Component
-	//virtual void UI() override {
-	///*	nk_checkbox_label(ctx, "IsPerspective", &isPerspective);
-	//	power = nk_propertyi(ctx, "Power", 0, power, 10000, 1.f, 1.f);
-	//	fov = nk_propertyf(ctx, "Fov", 30.f, fov, 120.0f, 0.01f, 0.005f);
-	//	nearClippingPlane = nk_propertyf(ctx, "Near", 0.001, nearClippingPlane, farClippingPlane, 1.f, 0.005f);
-	//	farClippingPlane = nk_propertyf(ctx, "Far", nearClippingPlane, farClippingPlane, 6000, 1.f, 0.005f);
-	//	speed = nk_propertyf(ctx, "Speed", 50, speed, 500, 1.f, 0.005f);
-	//	sensitivity = nk_propertyf(ctx, "Sensitivity", 0.001f, sensitivity, 3, 00.1f, 0.005f);*/
-	//}
-
 	virtual void HandleEvent(const SDL_Event &e) override;
 
-	void updateCameraVectors();
 	virtual Mat4& GetView();
 	virtual Mat4& GetProjection(ProjectionType type, int w, int h);
 };
